855aa09faad5be3bb671e059be5250f8bd70a384
Max <maxime_gagnon4@msn.com>
2017-04-01 16:55:11 -0400

    Ajout D'une partie de la vue 1

--- JeuPendu/src/JeuPendu.java
+++ JeuPendu/src/JeuPendu.java
@@ -1,9 +1,16 @@
 
+import java.awt.*;
 import java.awt.event.*;
+import javax.swing.*;
 
 
 
 public class JeuPendu extends WindowAdapter implements ActionListener {
+   
+    public static final int LARGEUR = 500;
+    public static final int HAUTEUR = 510;
+    public static final Font FONT_TITRE = new Font("Courier",Font.BOLD, 24);
+    
     public static final String PENDU_ZERO_ERREUR ="   _____________   "
             +"\n     |        |    "+"\n     |        |    "
             +"\n     |             "+"\n     |             "
@@ -44,7 +51,43 @@ public class JeuPendu extends WindowAdapter implements ActionListener {
     
     private int nbEssais = 0; // le nb d'essais effectuée, on pourra se servir de cette variable pour faire les affichage du pendu.
     
+    //Vue 1:
+    private JFrame fenetre;
+    private JLabel titre;
+    private JButton boutonJouer;
+    private JButton boutonStats;
+     
+    public JeuPendu() {
+        initVue1();
+    }
     
+    
+    private void initVue1(){
+        fenetre = new JFrame("JEU DU PENDU");
+        fenetre.setBounds(700,250, HAUTEUR, LARGEUR);
+        fenetre.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        fenetre.setResizable(true);
+        fenetre.setLayout(null);
+        fenetre.getContentPane().setBackground(java.awt.Color.WHITE);
+        
+        titre = new JLabel ("Le jeu du pendu");
+        titre.setBounds(0, 0, LARGEUR, HAUTEUR/3);
+        titre.setHorizontalAlignment(SwingConstants.CENTER);
+        titre.setFont(FONT_TITRE);
+        
+        boutonJouer = new JButton("Jouer");
+        boutonJouer.setBounds(titre.getX(), titre.getY()+20, HAUTEUR, LARGEUR);
+        
+        boutonStats = new JButton("Statistiques");
+        boutonStats.setBounds(boutonJouer.getX(), boutonJouer.getY()+10, HAUTEUR, LARGEUR);
+        
+        fenetre.getContentPane().add(titre);
+        fenetre.getContentPane().add(boutonJouer);
+        fenetre.getContentPane().add(boutonStats);
+        fenetre.setVisible(true);
+        
+        
+    }
 
     @Override
     public void actionPerformed(ActionEvent e) {
@@ -61,12 +104,16 @@ public class JeuPendu extends WindowAdapter implements ActionListener {
     }
     
     public static void main (String[] params){
-        System.out.println("\n"+PENDU_ZERO_ERREUR);
-        System.out.println("\n"+PENDU_UNE_ERREUR);
-        System.out.println("\n"+PENDU_DEUX_ERREURS);
-        System.out.println("\n"+PENDU_TROIS_ERREURS);
-        System.out.println("\n"+PENDU_QUATRE_ERREURS);
-        System.out.println("\n"+PENDU_CINQ_ERREURS);
-        System.out.println("\n"+PENDU_SIX_ERREURS);
+        new JeuPendu();
+
+
+
+//System.out.println("\n"+PENDU_ZERO_ERREUR);
+//        System.out.println("\n"+PENDU_UNE_ERREUR);
+//        System.out.println("\n"+PENDU_DEUX_ERREURS);
+//        System.out.println("\n"+PENDU_TROIS_ERREURS);
+//        System.out.println("\n"+PENDU_QUATRE_ERREURS);
+//        System.out.println("\n"+PENDU_CINQ_ERREURS);
+//        System.out.println("\n"+PENDU_SIX_ERREURS);
     }
 }
